850fadc97652e43046384433153f0dbd0c17c891
kishore4646 <bskishore918@gmail.com>
2026-05-01 11:26:24 +0530

    Initial commit - AI Emotion Music Recommender

--- project/train_large.py
+++
@@ -1,7 +0,0 @@
-import src.model as m
-
-# Use the large dataset if present
-m.DATASET_PATH = m.DATASET_PATH.replace('dataset.csv','dataset_108k.csv')
-print('Using dataset:', m.DATASET_PATH)
-
-m.train_model(save=True)
